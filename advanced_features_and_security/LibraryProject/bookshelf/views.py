5b1df004c327a9c56fbc75367dca1e5f55b2bffe
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-03-02 17:20:05 +0000

    1. Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
-# Create your views here.
+@permission_required('bookshelf.can_view', raise_exception=True)
+def article_list(request):
+    articles = Article.objects.all()
+    return render(request, 'articles/list.html', {'articles': articles})
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_article(request):
+    # Logic to create an article
+    pass
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    # Logic to edit the article
+    pass
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_article(request, article_id):
+    article = get_object_or_404(Article, id=article_id)
+    article.delete()
+    return redirect('article_list')
