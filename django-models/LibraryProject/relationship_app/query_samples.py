f074731dba9ffd5199fd4c8c235072ad9267fc71
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-03-19 21:36:12 +0000

    0. Implementing Advanced Model Relationships in Django

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,14 +1,16 @@
 from .models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
-author = Author.objects.get(name="Mohammed")
+author_name = "Mohammed"
+author = Author.objects.get(name=author_name)
 books_1 = author.books.all()
 books_2 = Book.objects.filter(author=author)
 # does books_1 has the same items as books_2 ??
 
 # List all books in a library
-books_3 = Library.objects.get(name="Space").books.all()
+library_name = "space"
+books_3 = Library.objects.get(name=library_name).books.all()
 
 # Retrieve the librarian for a library
-library = Library.objects.get(name="Math")
+library = Library.objects.get(name=library_name)
 librarian = Librarian.objects.get(library=library)
