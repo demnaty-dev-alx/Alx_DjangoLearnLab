1b891b20cdf5e62adaf1ca836cf84dec6ce8c682
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-03-19 23:14:43 +0000

    2. Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from .models import Book
 from django.views.generic.detail import DetailView
+from .models import Library
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from .models import Library
-from .models import Book
 
 def list_books(request):
       """Retrieves all books and renders a template displaying the list."""
