836a88afe309b2fe5e85bed11620b03472d065b0
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-03-19 23:53:09 +0000

    2. Implementing User Authentication in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import list_books, LibraryDetailView, SignUpView
-from django.contrib.auth.views import LoginView, LogoutView
+from .views import list_books, LibraryDetailView
+from . import views
+from django.contrib.auth.views import LogoutView
+
+app_name = 'relationship_app'
 
 urlpatterns = [
     path('books/', list_books, name="books"),
     path('library/', LibraryDetailView.as_view(), name="library"),
-    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', SignUpView.as_view(), name='register')
+    path('login/', views.user_login, name='login'),
+    path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('register/', views.register, name='register')
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Book
-from django.views.generic.detail import DetailView
 from .models import Library
-from django.views.generic import CreateView
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
 from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 def list_books(request):
       """Retrieves all books and renders a template displaying the list."""
@@ -18,7 +16,28 @@ class LibraryDetailView(DetailView):
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
 
-class SignUpView(CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/register.html'
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect("relationship_app:books")
+    elif request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("relationship_app:books")
+    else:
+        form = AuthenticationForm()
+    return render(request, "registration/login.html", {"form": form})
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect("relationship_app:books")
+    elif request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "registration/register.html", {"form": form})
