9e9089971c4128af530059929f5836936f9a0a2b
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-03-20 09:20:59 +0000

    3. Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -6,7 +6,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 def is_admin(user):
     return user.userprofile.role == 'Admin'
 
-class Admin(LoginRequiredMixin, View):
+class AdminView(LoginRequiredMixin, View):
     @user_passes_test(is_admin)
     def dispatch(self, request, *args, **kwargs):
         return super().dispatch(request, *args, **kwargs)
