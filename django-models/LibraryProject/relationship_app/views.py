43cd33e6da0d55637ccf04d13d4955a93b0bc4c8
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-03-20 10:20:17 +0000

    4. Implementing Custom Permissions in Django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import permission_required, user_passes_test
 
 def list_books(request):
       """Retrieves all books and renders a template displaying the list."""
