c7a1615909385029ac22402569c8895ebd479945
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-03-23 22:08:53 +0000

    3. Implementing Authentication and Permissions in Django REST Framework

--- api_project/api/permissions.py
+++ api_project/api/permissions.py
@@ -4,4 +4,4 @@ class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in ['GET', 'HEAD', 'OPTIONS']:
             return True
-        return obj.author == request.user
+        return obj.author == request.user.username
