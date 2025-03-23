66e73e3f29f348fa5b2a0f0bb3c9225fcf234574
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-03-23 22:43:16 +0000

    0. Setting Up a New Django Project with Custom Serializers in Django REST Framework

--- advanced-api-project/api/admin.py
+++ advanced-api-project/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Author, Book
 
-# Register your models here.
+# Register the Author model
+admin.site.register(Author)
+
+# Register the Book model
+admin.site.register(Book)
