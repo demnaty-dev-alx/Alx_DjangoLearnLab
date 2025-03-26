8a2274e2e8516542d9ef8d93b9449c903e654e15
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-03-26 03:38:25 +0000

    3. Adding Comment Functionality to Blog Posts

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -81,10 +81,6 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     form_class = PostForm
     template_name = 'blog/post_form.html'  # Reuse the same form template
 
-    def form_valid(self, form):
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
     def test_func(self):
         post = self.get_object()
         return self.request.user == post.author  # Allow only the post author to edit
