de4e8c99fc7bec06c87e8efc3f20dfc03381ea94
demnaty-dev <49480217+demnaty-dev@users.noreply.github.com>
2025-03-28 22:25:55 +0000

    2. Implementing User Follows and Feed Functionality

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,20 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework_nested.routers import NestedDefaultRouter
 from .views import PostViewSet, CommentViewSet, FeedView
 
-
-# Main Router (For Posts)
+# Create a router and register our viewsets with it
 router = DefaultRouter()
 router.register('posts', PostViewSet, basename='post')
-
-# Nested Router (For Comments inside Posts)
-post_router = NestedDefaultRouter(router, 'posts', lookup='post')  # 'post' will be used in URLs
-post_router.register('comments', CommentViewSet, basename='post-comments')
+router.register('comments', CommentViewSet, basename='comment')
 
 # The API URLs are now automatically determined by the router
 urlpatterns = [
     path('', include(router.urls)),
-    path('', include(post_router.urls)),
     path('feed/', FeedView.as_view(), name='user-feed'),
 ]
